cbfad375b10e1f14fc9f596544e2aa39292e1822
Duda Nogueira <dudanogueira@gmail.com>
2023-09-05 11:06:14 -0300

    point import to soruce folder add finish message

--- node-app/src/index.ts
+++ node-app/src/index.ts
@@ -7,6 +7,12 @@ const run = async () => {
   // await deleteCollection(collectionName);
   await createBindCollection(collectionName);
   await importMediaFiles(collectionName);
+  console.log("######################")
+  console.log("######################")
+  console.log("All set! You can now browse to http://localhost:4200")
+  console.log("######################")
+  console.log("######################")
+  
 }
 
 run();
